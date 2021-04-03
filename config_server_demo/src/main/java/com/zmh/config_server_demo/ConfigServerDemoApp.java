8112a8034510d449b5a017e5fe4abeb50d318ffc
ZMH_MOBILE <623299342@qq.com>
2021-04-03 17:03:48 +0800

    config repo not the name

--- config_server_demo/src/main/java/com/zmh/config_server_demo/ConfigServerDemoApp.java
+++ config_server_demo/src/main/java/com/zmh/config_server_demo/ConfigServerDemoApp.java
@@ -1,13 +1,27 @@
 package com.zmh.config_server_demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerDemoApp {
+    @Value("${git_username}")
+    String username;
+    @Value("${git_password}")
+    String password;
+
+    @GetMapping("git")
+    public Object git() {
+        System.out.println(username + " "+password);
+        return "git";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerDemoApp.class, args);
     }
